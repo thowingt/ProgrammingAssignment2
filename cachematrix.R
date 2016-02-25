d01b83c85a4d91076c7a03a545838311ebad0659
thowingt <thowingt@veriskclimate.com>
2016-02-25 10:48:38 -0500

    edited comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,41 @@
-
-## Put comments here that give an overall description of what your
-## functions do
-
-##  Creates a special "matrix" object that can cache 
-##  its inverse
+# makeCacheMatrix
+# Creates a special "matrix" object that can cache its inverse.
+# To start, set inv (the inverse of x) to NULL.
+# If setting the value of the matrix, set x to this value (y), and set inv to NULL.
+# If getting the value of the matrix, return x.
+# If setting the inverse, make inv equal to the inverse of x.
+# If retrieving the inverse, return inv.
+# Return a vector of functions that set or get the value of x, or set or get the inverse of x.
 
 makeCacheMatrix <- function(x = matrix()) {
-  #set inv (the inverse of x) to NULL
   inv <- NULL
-  # if setting the value, set x to this value (y), and set inv to NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  # if getting the value of the matrix, return x
   get <- function() x
-  # if setting the inverse, make inv equal to the inverse of x
   setinv <- function(solve) inv <<- solve
-  # if retrieving the inverse, return inv
   getinv <- function() inv
-  # return a vector of functions that set or get the value of x, or set or get the inverse of x
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
-## Computes the inverse of the special "matrix" returned
-## by makeCacheMatrix above. If the inverse has already 
-## been calculated (and the matrix has not changed), 
-## then cacheSolve should retrieve the inverse from the cache.
+# cacheSolve
+# Computes the inverse of the special "matrix" returned
+# by makeCacheMatrix above. If the inverse has already 
+# been calculated (and the matrix has not changed), 
+# then cacheSolve should retrieve the inverse from the cache.
+# Set inv to the value from makeCacheMatrix.  It may ne NULL.
+# If the value of inv is not NULL, use it and return it.
+# If the value of the inverse is NULL, find the inverse, set it, and return it.
 
 cacheSolve <- function(x, ...) {
-  # set inv to the value that has already been calulated
   inv <- x$getinv()
-  # if the value of inv is not NULL, use it and return it
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  # if there is not already an inverse, find the inverse, set it, and return it
   data <- x$get()
   inv <- solve(data, ...)
   x$setinv(inv)
